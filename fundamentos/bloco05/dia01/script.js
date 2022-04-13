a945aa64fdbb55713734035955d6a746499333d2
Rikelmy Lopes <rikelmylopes899@gmail.com>
2022-04-13 19:21:27 -0300

    atualizando atividades

---
+++ fundamentos/bloco05/dia01/script.js
@@ -0,0 +1,19 @@
+//blocos
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+var elementos = document.getElementsByClassName('emergency-tasks')[0];
+elementos.style.backgroundColor = 'pink';
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
+
+//linhas das palavras
+
+document.getElementsByClassName('urgent')[0].style.backgroundColor = 'purple';
+
+
+document.getElementsByClassName('urgent')[1].style.backgroundColor = 'purple';
+
+document.getElementsByClassName('no-urgent')[0].style.backgroundColor = 'black';
+
+
+document.getElementsByClassName('no-urgent')[1].style.backgroundColor = 'black';
