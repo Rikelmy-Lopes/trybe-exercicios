37e2a11e5a58af1a650049e35f65f753fd3a209a
Rikelmy Lopes <rikelmylopes899@gmail.com>
2022-06-10 15:55:13 -0300

    atividades do dia 10.3

---
+++ front-end/bloco10/dia01/my-project2/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return <footer className="content"><h1>Isso é so o começo...</h1></footer>
+    }
+}
+
+export default Footer
---
+++ front-end/bloco10/dia01/my-project2/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+      return <header className="content2"><h1>Conteudos de Front-End</h1></header>
+  }
+}
+
+
+export default Header
