d29da15438720547f9d5c7b9a0eebe82d8d08d56
Rikelmy-Lopes <rikelmylopes899@gmail.com>
2022-06-20 19:48:28 -0300

    atividades dia 10.2

---
+++ front-end/bloco10/dia02/my-pokedex/src/Pokedex.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Pokemon from "./Pokemon";
+
+import data from './data';
+
+class Pokedex extends React.Component{
+    render() {
+        return(
+    data.map((pokemon) => <Pokemon name={pokemon.name} type={pokemon.type} averageWeight={pokemon.averageWeight.value} measurementUnit={pokemon.averageWeight.measurementUnit} image={pokemon.image}/>)
+        );
+    }
+}
+
+export default Pokedex;
